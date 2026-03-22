cb5b49e3279c3c8feff9b72c4312102fd8647a3e
HadurIII <rudah.reis@yahoo.com.br>
2026-03-22 14:43:36 -0300

    Commit teste

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class CarrinhoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Carrinhossssssss',
+      title: 'Carrinhos',
       theme: ThemeData(primarySwatch: Colors.green),
       home: CarrinhosPage(),
     );
